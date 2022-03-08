6af4c619fa3ceb587ac2828c16fe9bdeeb2952bd
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-08 20:37:15 +0000

    exercicio if else

---
+++ bloco-4-introducao-a-javascript/script.js
@@ -0,0 +1,13 @@
+//console.log ("imprima isto!")
+
+
+const notaP = 90;
+
+if(notaP>=80){
+    console.log("Parabéns você foi aprovado!");
+} else if (notaP<80 && notaP >=60) {
+    console.log("você está na lista de espera!");
+} else if (notaP<60){
+    console.log("você reprovou!");
+}
+
