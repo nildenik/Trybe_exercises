39b890c772dcace417a67a82b91ca9d550a83bde
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-09 17:56:42 +0000

    for

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -21,12 +21,34 @@
 //   console.log("const num3 é o maior número");
 // }
 
-const valor1 = -3;
+// const valor1 = -3;
 
-if (valor1 < 0) {
-  console.log("o valor é negativo!");
-} else if (valor1 > 0) {
-  console.log("O valor é positivo!");
-} else {
-  console.log("o valor é zero, neutro!");
+// if (valor1 < 0) {
+//   console.log("o valor é negativo!");
+// } else if (valor1 > 0) {
+//   console.log("O valor é positivo!");
+// } else {
+//   console.log("o valor é zero, neutro!");
+// }
+
+// const angulo1 = 10;
+// const angulo2 = 60;
+// const angulo3 = 60;
+
+// if(angulo1+angulo2+angulo3===180){
+//   console.log("temos um angulo de 180! triangulo")
+
+// } else{
+//   console.log("não é um triangulo!")
+
+// }
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i += 1){
+  console.log(groceryList[i]);
+}
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
 }
