40c94cc86bb8c6d136c333cee65a64617aedbfb5
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-09 18:42:02 +0000

    array e loopfor

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var soma = 0;
+for(let i = 0; i<numbers.length; i+=1){
+    soma += parseInt(numbers[i]);
+   
+
+}
+console.log (soma);
+
+var array = ["1", "2", "3"]; // exemplo de um array básico!
+
+var soma = 0;
+
+for (var x=0; x < array.length; x++) {
+
+ soma += parseInt(array[x]);t
+}
