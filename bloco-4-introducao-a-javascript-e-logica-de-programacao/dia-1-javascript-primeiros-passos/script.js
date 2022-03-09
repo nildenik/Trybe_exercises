b1db84e2f245047040f1e6feb6db929658f58ee9
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-09 15:25:16 +0000

    exercicio negativo positivo

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -9,14 +9,24 @@
 
 //}
 
-const num1 = 0;
-const num2 = 5;
-const num3 = 8;
+//const num1 = 0;
+//const num2 = 5;
+// const num3 = 8;
 
-if (num1 > num2 && num1 > num3) {
-  console.log("const num1 é o maior número");
-} else if (num2 > num1 && num2 > num3) {
-  console.log("const num2 é o maior número");
+// if (num1 > num2 && num1 > num3) {
+//   console.log("const num1 é o maior número");
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("const num2 é o maior número");
+// } else {
+//   console.log("const num3 é o maior número");
+// }
+
+const valor1 = -3;
+
+if (valor1 < 0) {
+  console.log("o valor é negativo!");
+} else if (valor1 > 0) {
+  console.log("O valor é positivo!");
 } else {
-  console.log("const num3 é o maior número");
+  console.log("o valor é zero, neutro!");
 }
