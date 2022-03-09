d938b8007294291cc29d3399263e57a8869e1a08
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-09 19:42:30 +0000

    media aritimetica, for e arrays

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -2,16 +2,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var soma = 0;
 for(let i = 0; i<numbers.length; i+=1){
     soma += parseInt(numbers[i]);
-   
-
+    
 }
-console.log (soma);
+var resultado = soma/10;
+console.log(resultado);
 
-var array = ["1", "2", "3"]; // exemplo de um array básico!
 
-var soma = 0;
+// var array = ["1", "2", "3"]; // exemplo de um array básico!
 
-for (var x=0; x < array.length; x++) {
+// var soma = 0;
 
- soma += parseInt(array[x]);t
-}
+// for (var x=0; x < array.length; x++) {
+
+//  soma += parseInt(array[x]);t
+// }
+//<h2>A Média das notas é: ${somar()/notas.length}
