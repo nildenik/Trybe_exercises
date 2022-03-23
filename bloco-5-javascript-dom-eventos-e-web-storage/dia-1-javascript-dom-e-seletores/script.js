f8ad75c99247935521a595a19187d17770da210d
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-23 18:20:10 +0000

    trocando elementos

--- bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,8 +1,16 @@
-const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color = "red";
 
-console.log(document.getElementById("page-title").innerText = "The lord of the rings");
-document.getElementById("secondParagraph").innerText = "Este paragrafo vou colocar uma fonte diferente e mudar a cor";
-secondParagraph.style.color = "blue";
-document.getElementById("subtitle").innerText = "Este agora é o subtitulo alterado"
+// document.getElementById("page-title").innerText = "The lord of the rings";
+// document.getElementById("secondParagraph").innerText = "Este paragrafo vou colocar uma fonte diferente e mudar a cor";
+// secondParagraph.style.color = "blue";
+// document.getElementById("subtitle").innerText = "Este agora é o subtitulo alterado"
+
+// No getElementsByClassName é preciso passar a posição [0]
+
+let doisParagrafos = document.getElementsByClassName("classeTeste");
+
+for (var i = 0; i < doisParagrafos.length; i+=1){
+    doisParagrafos[i].innerText = "trocando os elemnetos";
+}
 
