b8227ded8ae72993f998adedb558acce3de9948a
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-24 17:40:29 +0000

    exercicio 5.1 finalizado

--- bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -11,6 +11,28 @@
 
     console.log (document.querySelectorAll("p"));
 
+// parentNode : retorna o nó pai.
+// parentElement : retorna o elemento pai.
+// childNodes : retorna um NodeList com todos os nós filhos.
+// children : retorna um HTMLCollection com todos os elementos filhos.
+// firstChild : retorna o primeiro nó filho.
+// firstElementChild : retorna o primeiro elemento filho.
+// lastChild : retorna o último nó filho.
+// lastElementChild : retorna o último elemento filho.
+// nextSibling : retorna o próximo nó.
+// nextElementSibling : retorna o próximo elemento.
+// previousSibling : retorna o nó anterior.
+// previousElementSibling : retorna o elemento anterior.
+// documentElement	Captura o elemento raiz <html> de um documento HTML.
+// getElementById	Busca um elemento da página Web com o uso do atributo id do elemento.
+// createElement	Cria um nodo elemento na página.
+// createAttribute	Cria um nodo atributo na página.
+// createTextNode	Cria um nodo texto na página.
+// getElementsByTagName	Retorna um array dos elementos com o mesmo nome.
+// appendChild	Insere um novo elemento filho.
+// removeChild	Remove um elemento filho.
+// parentNode	Retorna o nodo pai de um nodo.
+
 
 
 
