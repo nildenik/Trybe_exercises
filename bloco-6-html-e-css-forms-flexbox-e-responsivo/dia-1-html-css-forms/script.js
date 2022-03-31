ab994d6f5649d1a2516caa666a45075064d1504e
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-03-31 15:02:57 +0000

    preventDefault()

---
+++ bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,34 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+let eventoClique = HREF_LINK.addEventListener('click',clickEvento)
+
+function clickEvento(clique){
+clique.preventDefault();
+}
+
+
+let caixaBox = INPUT_CHECKBOX.addEventListener('click',eventoNulo)
+
+function eventoNulo(event){
+    event.preventDefault();
+
+}
+
+let eventoKeyPress = INPUT_TEXT.addEventListener('keypress',separaLetra).eventoKeyPress = 'a'
+
+function separaLetra (a){
+a.preventDefault()
+}
+
+
+
+
+
+
+
+// 1 - Adicione um evento click no elemento a que possui o id href . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
+// 2 - Adicione um evento click no elemento checkbox que possui o id input-checkbox . Na função do evento, implemente o método .preventDefault() para previnir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
+// 3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text . Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra 'a' minúscula) possa ser digitado na caixinha. ( dica: para capturar a tecla digitada, utilize o event.key )
