bc884a2729a9e41f1e787121cc559683635cabda
Nilde Lima <nildelima@ThinkBook15G2ITL5c58e893.lan>
2022-05-02 21:17:24 +0000

    exercicios

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,49 +1,95 @@
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// var soma = 0;
-// for (let i = 0; i < numbers.length; i += 1) {
-//   soma += parseInt(numbers[i]);
-// }
-// var resultado = soma / 10;
+// let lista = ['tormar café', 'reunião', 'sair com cachorro']
+ 
+// lista.pop()
+// console.log(lista);
 
-// if (resultado > 20) {
-//   console.log("A valor da média foi maior que 20");
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// // console.log(menuServices);
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu[2];
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('contato')
+// console.log(menu);
+// ---------------------------------------------------
+
+    // const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+    // for(let i = 0; i < groceryList.length; i+= 1){
+    //     console.log(groceryList[i]);
+    // }
+
+// const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for( const list of groceryList){
+//     console.log(list);
+// }
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+// let word = 'Hello';
+// for (let letter of word) {
+//   console.log(letter);
+// }
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+// const names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for(const imprimaLista of names ){
+//     console.log(imprimaLista);
+
+// }
+// _____________________________________________________
+// (Não pode ser uma const porque os números vão mudando)
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for(let i = 0; i < numbers.length; i += 1){
+//     result += numbers[i];
+// }
+// let media = result / numbers.length;
+
+// if(media<=20){
+//     console.log('Valor menor ou igual 20!')
 // } else {
-//   console.log("O valor foi menor que 20");
-//}
-
-// var array = ["1", "2", "3"]; // exemplo de um array básico!
-
-// var soma = 0;
-
-// for (var x=0; x < array.length; x++) {
-
-//  soma += parseInt(array[x]);t
-// }
-//<h2>A Média das notas é: ${somar()/notas.length}
+//     console.log('Valor maior de 20!')
+// _____________________________________________
 
 // let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-// //console.log(numbers[numbers.length - 1]);
-
-// for (let i = 0; i < numbers.length; i += 1) {
-//   console.log(numbers[i]);
-//}
-
-// ****************** NÃO RESOLVIDO ****************
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let soma = 0;
-
-// for(let i = 0; i < numbers.length; i--){
-//   console.log(numbers[i]);
+// let maiorNumero = numbers[0];
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[i] > maiorNumero){
+//         maiorNumero = numbers[i];
+//     }
 // }
+// console.log(maiorNumero);
 
-let numeroMega = [22, 14, 34, 16, 10, 11];
-let meusNumeros = [13, 12, 23, 24, 10, 11];
-for (let index = 0; index < numeroMega.length; index += 1) {
-  //console.log("Mega: ", numeroMega[index]);
-  for (let i = 0; i < meusNumeros.length; i += 1) {
-    if(numeroMega[index]=== meusNumeros[i]){
-    console.log("Parabéns você acertou! ", meusNumeros[i]);
-  }
-  }
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[i] %2 !== 0){
+//         result += 1;
+//     }  
+// }
+// if (result === 0) {
+//     console.log('nenhum valor ímpar encontrado');
+//   } else {
+//     console.log(result);
+
+// }
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let menorNumero = numbers[0];
+
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[i] < menorNumero){
+//         menorNumero = numbers[i];
+//     }
+// }
+// console.log(menorNumero);,
