1da840919cdf8b96014d8aff5dc3688255f769de
nildenik <nildenik@gmail.com>
2022-05-17 13:19:31 +0000

    exercicios sort

--- bloco-8-hofsHigher Order Functions - forEach, find, some, every, sort/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ bloco-8-hofsHigher Order Functions - forEach, find, some, every, sort/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -87,7 +87,7 @@
   console.log(imprimeDados('Nilde', 30));
 
   // ---------------------------------------------------------------
-  
+
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const multipliesFor2 = (element) => {
@@ -95,3 +95,48 @@
   };
   
   numbers.forEach(multipliesFor2);
+  // -------------------------------------------------------------
+  const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  };
+  
+  emailListInData.forEach(showEmailList);
+  // ----------------------------------------------------------------
+  const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); 
+// ------------------------------------------------------------------
+// neste exercicio o sort ordena pela idade.
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort(((a, b) => a.name - b.name));
+
+console.log(people);
+// ----------------------------------------------
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  // Adicione seu código aqui:
+}
+
+console.log(findNameWithFiveLetters());
+
