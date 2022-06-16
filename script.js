53e72d68b8e6514fd2dbd1da6f1d548c00329721
Nilde Lima <nildenik@gmail.com>
2022-06-16 19:21:48 +0100

    calcula fatorial

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 
-const contVerde = document.getElementById('header-container')
-.style.backgroundColor = 'green';
+function canculaFatorial(number) {
+    let numeroFatorial = 1;
+    let num = number;
+    if (number < 0) {
+        return 'Digite um numero inteiro';
+    }
+    for (let i = 1; i < number; i++) {
+        num = num * i;
+        numeroFatorial = num;
+    }
+    return numeroFatorial;
 
-const pintarRosa = document.getElementsByClassName('emergency-tasks')[0]
-.style.backgroundColor = 'rgb(255, 159, 132)';
-
-const hUrg = document.querySelectorAll('.emergency-tasks h3');
-for(let i = 0; i < hUrg.length; i ++){
-hUrg[i].style.backgroundColor = 'rgb(165, 0, 243)';
 }
-const pintarAmarelo = document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+console.log(canculaFatorial(5));
 
-const hNaoUrgen = document.querySelectorAll('.no-emergency-tasks h3');
-for(let index = 0; index < hNaoUrgen.length; index++){
-    hNaoUrgen[index].style.backgroundColor = 'black'
-}
 
